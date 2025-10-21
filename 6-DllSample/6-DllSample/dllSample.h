74cc16a3c9d271f9bb632bbae8ba6ef536124d9b
liqiang <liqiang@makeblock.com>
2025-10-21 17:46:15 +0800

    feat:DLL项目文档注释

--- 6-DllSample/6-DllSample/dllSample.h
+++ 6-DllSample/6-DllSample/dllSample.h
@@ -1,6 +1,12 @@
 #pragma once
 
 // 声明导出的变量、函数和类
+/*
+* 在函数声明前面加上
+* extern "C"关键字后，C++会对该函数强制使用标准C的函数名
+* 称修饰方式，不会对函数名、变量名进行改编，在DLL中用这
+* 种方式导出的函数可以被其他语言使用；
+*/
 #ifdef DLL_EXPORT
     #define DLLSAMPLE_VARABLE   extern "C" __declspec(dllexport)
     #define DLLSAMPLE_CLASS     __declspec(dllexport)
@@ -39,5 +45,15 @@ private:
 };
 
 // 导出函数
+/*
+* 当使用__stdcall来导出C函数时，编译器会对函数名进行改编，
+* 函数名前面有一个下划线“_”，然后是函数名，函数名后面是一
+* 个@符号后跟作为参数传递给函数的字节数组成
+*   _funAdd@8
+*   _funMul@8
+* 
+* 这种情况下，我们可以添加一个.def文件指示编译器导出
+* funAdd和funMul函数，而不是_funAdd@8和_funMul@8函数。
+*/
 DLLSAMPLE_API int WINAPI funAdd(int a, int b);
 DLLSAMPLE_API int WINAPI funMul(int a, int b);
