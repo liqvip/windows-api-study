31f2e97ebfb26a96836c1a79727ea8ca0206f514
liqiang <liqiang@makeblock.com>
2025-10-31 14:53:44 +0800

    feat:子窗口控件-Button

---
+++ Base-8-SubWindow-Button/Base-8-SubWindow-Button/resource.h
@@ -0,0 +1,36 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Base-8-SubWindow-Button.rc 使用
+//
+#define IDB_KONGLONG                    2
+#define IDI_ICON1                       102
+#define IDI_SMILE                       102
+
+#define IDC_PUSHBUTTON          1000
+#define IDC_ICONBUTTON          1001
+#define IDC_BITMAPBUTTON        1002
+#define IDC_OWNERDRAWBUTTON     1003
+#define IDC_GROUPBOX1           1004
+#define IDC_AUTORADIOBUTTON1    1005
+#define IDC_AUTORADIOBUTTON2    1006
+#define IDC_AUTORADIOBUTTON3    1007
+#define IDC_GROUPBOX2           1008
+#define IDC_AUTOCHECKBOX1       1009
+#define IDC_AUTOCHECKBOX2       1010
+#define IDC_AUTOCHECKBOX3       1011
+#define IDC_GROUPBOX3           1012
+#define IDC_AUTO3STATE1         1013
+#define IDC_AUTO3STATE2         1014
+#define IDC_AUTO3STATE3         1015
+#define IDC_DEFPUSHBUTTON       1016
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
